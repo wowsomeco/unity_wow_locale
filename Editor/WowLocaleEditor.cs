6dafa28cc2b7f2ad7e79628551acd59124d86510
jim <irawan.jemmy@gmail.com>
2020-09-16 23:02:58 +0800

    init

---
+++ Editor/WowLocaleEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Wowsome {
+  using EU = EditorUtils;
+  using LangModel = WowLocale.LangModel;
+  using LocaleModel = WowLocale.LocaleModel;
+
+  [CustomEditor(typeof(WowLocale))]
+  public class WowLocaleEditor : Editor {
+    Menu<LangModel> _languages = new Menu<LangModel>();
+    LangModel _selectedLanguage = null;
+    LocaleModel _selectedLocale = null;
+
+    public override void OnInspectorGUI() {
+
+      DrawDefaultInspector();
+      WowLocale tgt = (WowLocale)target;
+
+      _languages.Build(new Menu<LangModel>.BuildCallback(
+          "Languages",
+          tgt.Languages,
+          l => l.title,
+          s => {
+            _selectedLanguage = s.Model;
+            TextAsset t = Resources.Load<TextAsset>($"{tgt.Path}/locale_{_selectedLanguage.id}");
+            if (null == t) {
+              _selectedLocale = new LocaleModel(_selectedLanguage.id);
+            } else {
+              _selectedLocale = JsonUtility.FromJson<LocaleModel>(t.text);
+            }
+          }
+      ));
+
+      if (null != _selectedLocale) {
+        EditorGUILayout.LabelField(_selectedLocale.lang, EditorStyles.boldLabel);
+      }
+    }
+  }
+}
+
---
+++ scripts/WowLocale.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wowsome {
+  public class WowLocale : MonoBehaviour {
+    [Serializable]
+    public class LangModel {
+      public string id;
+      public string title;
+    }
+
+    [Serializable]
+    public class LocaleModel {
+      public string lang;
+      public List<LocaleTextModel> texts = new List<LocaleTextModel>();
+
+      public LocaleModel(string l) {
+        lang = l;
+      }
+    }
+
+    [Serializable]
+    public class LocaleTextModel {
+      public string key;
+      public string value;
+    }
+
+    public string Path;
+    public List<LangModel> Languages = new List<LangModel>();
+    public Dictionary<string, LocaleModel> Data = new Dictionary<string, LocaleModel>();
+
+    public string SelectedLang { get; set; }
+
+    public string DefaultLang {
+      get { return Languages[0].id; }
+    }
+
+    public void InitLocaleManager() {
+      TextAsset[] tas = Resources.LoadAll<TextAsset>(Path);
+      foreach (TextAsset t in tas) {
+        LocaleModel locale = JsonUtility.FromJson<LocaleModel>(t.text);
+        Data[locale.lang] = locale;
+      }
+    }
+
+    public string GetText(string key) {
+      if (SelectedLang.IsEmpty()) SelectedLang = Languages[0].id;
+
+      LocaleModel locale = Data[SelectedLang];
+      LocaleTextModel textModel = locale.texts.Find(x => x.key == key);
+      if (null != textModel) return textModel.value;
+
+      LocaleTextModel defaultTextModel = Data[DefaultLang].texts.Find(x => x.key == key);
+      return null != defaultTextModel ? defaultTextModel.value : null;
+    }
+  }
+}
+
